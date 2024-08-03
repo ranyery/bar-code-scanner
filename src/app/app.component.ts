7ee0bc4ddfa3d92d6651c5a35f97270eeb6310a1
ranyery <ranyerysc@gmail.com>
2024-08-03 07:09:38 -0300

    feat: add ngx-scanner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BarcodeFormat } from '@zxing/library';
+import { BehaviorSubject } from 'rxjs';
+
+// https://github.com/zxing-js/ngx-scanner/wiki/Advanced-Usage
+// https://github.com/zxing-js/ngx-scanner/blob/master/projects/zxing-scanner-demo/src/app/app.component.html
+// https://dribbble.com/shots/4363843-Bar-QR-code-Scanner
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'bar-code-scanner';
+export class AppComponent implements OnInit {
+  public torchEnabled: boolean = false;
+  public deviceCurrent?: MediaDeviceInfo;
+  public deviceSelected?: string;
+  public qrResultString?: string;
+  public tryHarder: boolean = false;
+  public hasPermission: boolean = false;
+  public availableDevices: MediaDeviceInfo[] = [];
+  public hasDevices: boolean = false;
+  public readonly torchAvailable$ = new BehaviorSubject<boolean>(false);
+
+  public readonly formatsEnabled: BarcodeFormat[] = [
+    BarcodeFormat.CODE_128,
+    BarcodeFormat.DATA_MATRIX,
+    BarcodeFormat.EAN_13,
+    BarcodeFormat.QR_CODE,
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public onDeviceChange(device: MediaDeviceInfo) {
+    console.log('onDeviceChange:', device);
+
+    const selectedStr = device?.deviceId || '';
+    if (this.deviceSelected === selectedStr) {
+      return;
+    }
+
+    this.deviceSelected = selectedStr;
+    this.deviceCurrent = device || undefined;
+  }
+
+  public onCodeResult(resultString: string) {
+    console.log('onCodeResult:', resultString);
+    this.qrResultString = resultString;
+  }
+
+  public onHasPermission(has: boolean) {
+    console.log('onHasPermission:', has);
+    this.hasPermission = has;
+  }
+
+  public onCamerasFound(devices: MediaDeviceInfo[]): void {
+    console.log('onCamerasFound:', devices);
+
+    this.availableDevices = devices;
+    this.hasDevices = Boolean(devices && devices.length);
+  }
+
+  public onTorchCompatible(isCompatible: boolean): void {
+    console.log('onTorchCompatible:', isCompatible);
+
+    this.torchAvailable$.next(isCompatible || false);
+  }
+
+  public onDeviceSelectChange(selected: string) {
+    debugger;
+    console.log('onDeviceSelectChange:', selected);
+
+    const selectedStr = selected || '';
+    if (this.deviceSelected === selectedStr) {
+      return;
+    }
+    this.deviceSelected = selectedStr;
+    const device = this.availableDevices.find((x) => x.deviceId === selected);
+    this.deviceCurrent = device || undefined;
+  }
 }
